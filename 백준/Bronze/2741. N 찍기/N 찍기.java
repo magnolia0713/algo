66560b50eeb2244fc19b07178adac55a0702e46e
magnolia0713 <dearmagnolia0713@gmail.com>
2026-01-08 23:33:49 +0900

    [Bronze V] Title: N 찍기, Time: 1100 ms, Memory: 33344 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
