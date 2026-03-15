f83c24f00ec083aef96cda733825773f65eda94d
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:58:33 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 32924 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,38 +1,31 @@
-n = int(input())
-n_list = list(map(int, input().split()))
-calc_list = list(map(int, input().split()))
-a_max = -1e11
-a_min = 1e11
-
-def dfs(depth, num):
-    global a_max, a_min
-    if depth == n-1:
-        if a_max < num:
-            a_max = num
-
-        if a_min > num:
-            a_min = num
+def dfs(depth, result):
+    if depth == len(num_list):
+        result_set.add(result)
         return
 
     for i in range(4):
-        if calc_list[i]:
-
-            calc_list[i] -= 1
+        if a_list[i] != 0:
+            a_list[i] -= 1
             if i == 0:
-                dfs(depth+1, num + n_list[depth+1])
-            if i == 1:
-                dfs(depth+1, num - n_list[depth+1])
-            if i == 2:
-                dfs(depth+1, num * n_list[depth+1])
-            if i == 3:
-                if num < 0:
-                    dfs(depth + 1, -(-num // n_list[depth+1]))
+                dfs(depth + 1, result + num_list[depth])
+            elif i == 1:
+                dfs(depth + 1, result - num_list[depth])
+            elif i == 2:
+                dfs(depth + 1, result * num_list[depth])
+            else:
+                if result < 0:
+                    dfs(depth + 1, -(-result // num_list[depth]))
                 else:
-                    dfs(depth + 1, num // n_list[depth+1])
+                    dfs(depth + 1, result // num_list[depth])
+            a_list[i] += 1
 
-            calc_list[i] += 1
 
-dfs(0, n_list[0])
 
-print(a_max)
-print(a_min)
+N = int(input())
+num_list = list(map(int, input().split()))
+a_list = list(map(int, input().split()))
+result_set = set()
+dfs(1, num_list[0])
+
+print(max(result_set))
+print(min(result_set))
