7eb19ae62fba1bf74760bc1876109157decd3e6c
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:22:12 +0900

    [Gold III] Title: 우주 탐사선, Time: 244 ms, Memory: 112124 KB -BaekjoonHub

---
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.py
@@ -0,0 +1,37 @@
+
+def dfs(start, value):
+    global a_min
+    if not visit_list:
+        if a_min > value:
+            a_min = value
+        return
+
+
+    for p in range(len(visit_list)):
+        q = visit_list.pop(p)
+        dfs(q, value + matrix[start][q])
+        visit_list.insert(p,q)
+
+
+n, m = map(int, input().split())
+check = n // 3 * 2
+INF = 10000000
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+for k in range(n):
+    for r in range(n):
+        for c in range(n):
+            matrix[r][c] = min(matrix[r][c], matrix[r][k] + matrix[k][c])
+
+
+
+visit_list = list(range(n))
+visit_list.remove(m)
+a_min = 10000000
+dfs(m,0)
+
+print(a_min)
+
+
+
