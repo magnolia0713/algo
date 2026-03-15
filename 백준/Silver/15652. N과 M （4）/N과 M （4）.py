52060478b2cb2aa8f475c5c51a5e3dfb4779d9fc
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 02:04:27 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+
+
+
+def NM1(N, M, list_a=None):
+    #for initiate
+    if list_a is None:
+        list_a = [[i] for i in range(1, N+1)]
+        M -= 1
+
+    if M == 0:
+        return list_a
+
+    next_list = []
+    for list_b in list_a:
+        for i in range(1, N+1):
+            if list_b[-1] <= i:
+                next_list.append(list_b + [i])
+    return NM1(N, M-1, next_list)                
+
+result = NM1(N, M)
+for i in result:
+    print(*i)
