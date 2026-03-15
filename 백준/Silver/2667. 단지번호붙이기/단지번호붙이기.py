151123ff1fc86634f45a95ed182e29eb4d01fa46
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:59:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,26 +1,31 @@
-def bfs():
-    count = 1
-    while basket:
-        n, m = basket.pop()
-        for dr, dc in dirs:
-            nr = n + dr
-            nc = m + dc
-            if 0 <= nr < N and 0 <= nc < N and matrix[nr][nc] == 1:
-                matrix[nr][nc] = 0
-                basket.append((nr,nc))
-                count += 1
-
-    return count
-
-dirs = [(0,1),(1,0),(-1,0),(0,-1)]
 N = int(input())
+
+def dfs(r, c):
+    for dr, dc in dir:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < N and 0 <= nc < N and check_point[nr][nc] and matrix[nr][nc] == 1:
+            check_point[nr][nc] = False
+            counter[0] += 1
+            dfs(nr, nc)
+    else: return
+
+#for dfs
 matrix = [list(map(int, list(input()))) for _ in range(N)]
-total_a = []
+check_point = [[True] * N for _ in range(N)]
+dir = [(1,0),(0,1),(0,-1),(-1,0)]
+list_temp = []
+list_size = []
 for r in range(N):
     for c in range(N):
-        if matrix[r][c] != 0:
-            matrix[r][c] = 0
-            basket = [(r,c)]
-            total_a.append(bfs())
-total_a.sort()
-print(len(total_a), *total_a, sep='\n')
+        if check_point[r][c] and matrix[r][c] == 1:
+            check_point[r][c] = False
+            counter = [1]
+            dfs(r,c)
+            if counter[0] != 0:
+                list_size.append(counter[0])
+        else:
+            check_point[r][c] = False
+
+list_size.sort()
+print(len(list_size))
+print(*list_size, sep='\n')
