8122cb3a107157be07d71c3eabb8b4cba1f32084
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:57:53 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -7,9 +7,14 @@ def calc(n):
         if n == 3:
             return 4
     else:
-        return calc(n-1) + calc(n-2) + calc(n-3)
+        if memo[n] == 0:
+            memo[n] = calc(n-1) + calc(n-2) + calc(n-3)
+            return memo[n]
+        else:
+            return memo[n]
 
 T = int(input())
 for tc in range(1, T+1):
+    memo = [0] * 11
     num = int(input())
     print(calc(num))
