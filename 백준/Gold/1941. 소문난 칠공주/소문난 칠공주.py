de1a453270930b1a7ecb3c1d1fd83e6e3f7245dc
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:57:04 +0900

    [Gold III] Title: 소문난 칠공주, Time: 612 ms, Memory: 115600 KB -BaekjoonHub

---
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -0,0 +1,41 @@
+def subseting(start=0, depth=0,cnt_y=0):
+    global total_count
+    if cnt_y == 4:
+        return
+
+    if depth < 7:
+        for i in range(start,25):
+            if matrix[i//5][i % 5] == 'Y':
+                subset.append(divmod(i,5))
+                subseting(i+1,depth+1,cnt_y+1)
+                subset.pop()
+            else:
+                subset.append(divmod(i,5))
+                subseting(i+1,depth+1,cnt_y)
+                subset.pop()
+
+    else:
+        basket = [tuple(subset[0])]
+        count_a = 1
+        check_list = [tuple(subset[0])]
+        while basket:
+            n, m = basket.pop()
+            for dr, dc in dirs:
+                nr = n + dr
+                nc = m + dc
+                if (nr,nc) in subset and (nr, nc) not in check_list:
+                    basket.append((nr,nc))
+                    check_list.append((nr,nc))
+                    count_a += 1
+
+            if count_a == 7:
+                total_count += 1
+                break
+#=================================================
+dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+matrix = [list(input()) for _ in range(5)]
+total_count = 0
+subset = []
+cnt_y = 0
+subseting()
+print(total_count)
