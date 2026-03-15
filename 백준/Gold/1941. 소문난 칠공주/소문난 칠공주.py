7a91874fc503cb61c7ae30b846e9a151aa70fd5d
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:57:09 +0900

    [Gold III] Title: 소문난 칠공주, Time: 564 ms, Memory: 113348 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,21 +1,18 @@
-def subseting(start=0, depth=0,cnt_y=0):
-    global total_count
-    if cnt_y == 4:
-        return
-
+def subseting(start=0, depth=0):
+    global count, total_count
     if depth < 7:
         for i in range(start,25):
-            if matrix[total_subset[i][0]][total_subset[i][1]] == 'Y':
-                subset.append(total_subset[i])
-                subseting(i+1,depth+1,cnt_y+1)
-                subset.pop()
-            else:
-                subset.append(total_subset[i])
-                subseting(i+1,depth+1,cnt_y)
-                subset.pop()
-
+            subset.append(total_subset[i])
+            subseting(i+1,depth+1)
+            subset.pop()
 
     else:
+        count = 0
+        for r, c in subset:
+            if matrix[r][c] == 'S':
+                count += 1
+        if count < 4:
+            return
         basket = [tuple(subset[0])]
         count_a = 1
         check_list = [tuple(subset[0])]
@@ -41,6 +38,6 @@ visited = [[0] * 5 for _ in range(5)]
 total_count = 0
 total_subset = [(i,j) for i in range(5) for j in range(5)]
 subset = []
-count = cnt_y = 0
+count = 0
 subseting()
 print(total_count)
