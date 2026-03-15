eeb164896d796e18794e12d2685083fb87e0e6d5
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:18:34 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 512 ms, Memory: 258820 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,26 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+def indexing(start, end, num):
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if a_list[mid] < num:
+            start = mid + 1
+
+        else:
+            end = mid - 1
+
+    return start if start < len(a_list) else -1
+
+a_list = [n_list[0]]
+
+for i in range(1, len(n_list)):
+    idx = indexing(0, len(a_list) - 1, n_list[i])
+
+    if idx == -1:
+        a_list.append(n_list[i])
+    else:
+        a_list[idx] = n_list[i]
+print(len(a_list))
