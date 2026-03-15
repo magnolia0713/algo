362817627625da0e7e20095bc0b2a3e1fd32102b
dongryun <dearmagnolia0713@gmail.com>
2026-03-16 01:59:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,31 +1,25 @@
 N = int(input())
-
-def dfs(r, c):
-    for dr, dc in dir:
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < N and 0 <= nc < N and check_point[nr][nc] and matrix[nr][nc] == 1:
-            check_point[nr][nc] = False
-            counter[0] += 1
-            dfs(nr, nc)
-    else: return
-
-#for dfs
 matrix = [list(map(int, list(input()))) for _ in range(N)]
-check_point = [[True] * N for _ in range(N)]
-dir = [(1,0),(0,1),(0,-1),(-1,0)]
-list_temp = []
+check_list = []
+list_a = []
 list_size = []
+matrix_visited = [[True] * N for _ in range(N)]
+
 for r in range(N):
     for c in range(N):
-        if check_point[r][c] and matrix[r][c] == 1:
-            check_point[r][c] = False
-            counter = [1]
-            dfs(r,c)
-            if counter[0] != 0:
-                list_size.append(counter[0])
-        else:
-            check_point[r][c] = False
+        if matrix[r][c] == 1 and [r,c] and matrix_visited[r][c]:
+            list_a.append([r,c])
+            matrix_visited[r][c] = False
 
+            for i, j in list_a:
+                for di, dj in [[1,0],[0,1],[-1,0],[0,-1]]:
+                    ni, nj = i+di, j+dj
+                    if 0 <= ni < N and 0 <= nj < N and matrix[ni][nj] == 1 and matrix_visited[ni][nj]:
+                        list_a.append([ni,nj])
+                        matrix_visited[ni][nj] = False
+            check_list.extend(list_a.copy())
+            list_size.append(len(list_a))
+            list_a.clear()
 list_size.sort()
 print(len(list_size))
 print(*list_size, sep='\n')
